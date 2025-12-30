0a1d3db0958647d875d3e77b851d20de15ecc3e4
Paulo <pauloilarde@gmail.com>
2025-12-30 09:46:49 -0300

    Criando Estrutura básica; Definindo constantes MESES abreviados em pt-BR; Importando datetime e random

---
+++ 02_birthday_paradox/birthday_paradox.py
@@ -0,0 +1,18 @@
+"""
+Paradoxo do aniversário
+Maiores informações: https://pt.wikipedia.org/wiki/Paradoxo_do_anivers%C3%A1rio
+Baseado no projeto de Al Sweigart
+"""
+
+import datetime
+import random
+
+MESES = ('Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez')
+
+def main():
+  """Função principal"""
+  # TODO: implementar lógica
+  pass
+
+if __name__ == "__main__":
+  main()
